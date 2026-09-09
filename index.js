89daeabe81d10c796a5339c6ccb3da5e6e40d042
mateustavaresw <mateustavareszx007@gmail.com>
2026-09-09 18:16:19 -0300

    cria função que verifica palavras duplicadas

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
 const fs = require('fs')
 fs.readFile(link, 'utf-8', (erro,texto)=>{
-    verificarDuplicadas(texto)
+    verificarPalavras(texto)
 })
+//criar um array de palavras
+//contar as ocorrencias
+//montar objeto com resultado
+// {
+//     "web": 5,
+//     "computador: 4",
+// }
+function verificarPalavras (texto){
+    const listaPalavras = texto.split(' ')
+    const resultado = {};
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0 ) + 1
+    });
+    console.log(resultado)
+}
+
