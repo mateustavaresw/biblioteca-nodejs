a1fda0a8df5b3ffc21bba55fcded4f1c4b57fd11
mateustavaresw <mateustavareszx007@gmail.com>
2026-09-09 18:32:13 -0300

    cria função quebra de paragrafos e limpa caracteres especiais

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
 const fs = require('fs')
 fs.readFile(link, 'utf-8', (erro,texto)=>{
-    verificarPalavras(texto)
+    quebraParagrafos(texto)
 })
 //criar um array de palavras
 //contar as ocorrencias
@@ -12,12 +12,25 @@ fs.readFile(link, 'utf-8', (erro,texto)=>{
 //     "web": 5,
 //     "computador: 4",
 // }
+function limpaPalavras(palavra){
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
 function verificarPalavras (texto){
     const listaPalavras = texto.split(' ')
     const resultado = {};
     listaPalavras.forEach(palavra => {
-        resultado[palavra] = (resultado[palavra] || 0 ) + 1
+        if (palavra.length >= 3){
+            const palavraLimpa = limpaPalavras(palavra);
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0 ) + 1
+        }
     });
-    console.log(resultado)
+    return resultado;
+}
+function quebraParagrafos (texto){
+    const paragrafos = texto.toLowerCase().split('\n')
+    const contagem = paragrafos.map((paragrafo)=>{
+        return verificarPalavras(paragrafo)
+    })
+    console.log(contagem)
 }
 
