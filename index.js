273873c522c7ba12bcc30e423cdde65388b4c758
mateustavaresw <mateustavareszx007@gmail.com>
2026-09-15 17:23:13 -0300

    refinamento da contagem e adicionando tratamento de erro

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-
-const caminhoArquivo = process.argv
-const link = caminhoArquivo[2]
-const fs = require('fs')
-fs.readFile(link, 'utf-8', (erro,texto)=>{
-    quebraParagrafos(texto)
-})
-//criar um array de palavras
-//contar as ocorrencias
-//montar objeto com resultado
-// {
-//     "web": 5,
-//     "computador: 4",
-// }
-function limpaPalavras(palavra){
-    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
+const fs = require("fs");
+fs.readFile(link, "utf-8", (erro, texto) => {
+  try {
+    if (erro) throw erro
+    contaPalavra(texto);
+  } catch (erro) {
+    if (erro.code === "ENOENT") console.log("erro que esperava");
+    else console.log("outro erro");
+  }
+});
+function contaPalavra(texto) {
+  const paragrafos = extraiParagrafos(texto);
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificarPalavras(paragrafo);
+  });
+  console.log(contagem);
 }
-function verificarPalavras (texto){
-    const listaPalavras = texto.split(' ')
-    const resultado = {};
-    listaPalavras.forEach(palavra => {
-        if (palavra.length >= 3){
-            const palavraLimpa = limpaPalavras(palavra);
-            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0 ) + 1
-        }
-    });
-    return resultado;
+function extraiParagrafos(texto) {
+  return texto.toLowerCase().split("\n");
 }
-function quebraParagrafos (texto){
-    const paragrafos = texto.toLowerCase().split('\n')
-    const contagem = paragrafos.map((paragrafo)=>{
-        return verificarPalavras(paragrafo)
-    })
-    console.log(contagem)
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 }
-
+function verificarPalavras(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+  return resultado;
+}
